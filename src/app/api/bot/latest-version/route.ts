811313c8f63ea68025b5593ec96de85d7d9ab395
Wahyu Budiman <wahyubudiman0624@gmail.com>
2026-08-20 21:34:24 +0700

    feat: add SHA-256 hash to bot updater manifest

--- src/app/api/bot/latest-version/route.ts
+++ src/app/api/bot/latest-version/route.ts
@@ -9,6 +9,7 @@ export async function GET() {
   // Bisa diganti valuenya secara manual di sini saat ada update baru
   return NextResponse.json({
     latest_version: "1.0.1",
+    hash: "e3824d2c61b200d89900edf6b84b0aa8e197d7613bf40f1ea25ce9b16cabede8",
     download_url: "https://url-tempat-kamu-menaruh/OnmyojiFarmBotQt_WGC_Fixed.exe"
   });
 }
